bb455badfa197dd4c9423bd4c80b9f0038a63564
tanerdambasan <taner.dambasan@gmail.com>
2026-08-29 14:24:52 +0300

    demo üzerinden şirket olusturma bugfix

--- server.js
+++ server.js
@@ -130,6 +130,7 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrcAttr: ["'unsafe-inline'"],
       styleSrc: ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
       fontSrc: ["'self'", 'https://fonts.gstatic.com', 'data:'],
       imgSrc: ["'self'", 'data:', 'https:'],
